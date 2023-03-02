e69394b0e735973764c92af8015b269bdd9740eb
ryomen666 <113868184+ryomen666@users.noreply.github.com>
2023-03-02 21:29:59 +0900

    改良

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,78 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "@/styles/News.module.css";
 
-const News = () => {
+const API_KEY = process.env.API_KEY;
+
+export default function Home() {
   const [category, setCategory] = useState("general");
-  const [country, setCountry] = useState("us");
-  const [news, setNews] = useState([]);
+  const [country, setCountry] = useState("jp");
+  const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const res = await fetch(
-        `https://newsapi.org/v2/top-headlines?category=${category}&country=${country}&apiKey=68ad059b799d401b83fef24e593b8986`
-      );
-      const data = await res.json();
-      setNews(data.articles);
-    };
-    fetchNews();
-  }, [category, country]);
-
-  const handleCategoryChange = (event) => {
+  const handleChangeCategory = (event) => {
     setCategory(event.target.value);
   };
 
-  const handleCountryChange = (event) => {
+  const handleChangeCountry = (event) => {
     setCountry(event.target.value);
+  };
+
+  const handleClick = async () => {
+    const res = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`
+    );
+    const data = await res.json();
+    setArticles(data.articles);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.controls}>
-        <label>
-          Category:
-          <select value={category} onChange={handleCategoryChange}>
-            <option value="general">General</option>
-            <option value="business">Business</option>
-            <option value="entertainment">Entertainment</option>
-            <option value="health">Health</option>
-            <option value="science">Science</option>
-            <option value="sports">Sports</option>
-            <option value="technology">Technology</option>
-          </select>
-        </label>
-        <label>
-          Country:
-          <select value={country} onChange={handleCountryChange}>
-            <option value="us">US</option>
-            <option value="ca">Canada</option>
-            <option value="jp">Japan</option>
-            <option value="gb">UK</option>
-            <option value="au">Australia</option>
-          </select>
-        </label>
+      <h1>Google News</h1>
+      <div>
+        <select value={category} onChange={handleChangeCategory}>
+          <option value="general">総合</option>
+          <option value="business">ビジネス</option>
+          <option value="entertainment">エンタメ</option>
+          <option value="health">健康</option>
+          <option value="science">科学</option>
+          <option value="sports">スポーツ</option>
+          <option value="technology">テクノロジー</option>
+        </select>
+        <select value={country} onChange={handleChangeCountry}>
+          <option value="jp">日本</option>
+          <option value="us">アメリカ</option>
+          <option value="gb">イギリス</option>
+          <option value="au">オーストラリア</option>
+          <option value="ca">カナダ</option>
+          <option value="fr">フランス</option>
+          <option value="de">ドイツ</option>
+        </select>
+        <button onClick={handleClick}>表示</button>
       </div>
-      <div className={styles.cardContainer}>
-        {news.map((article, index) => (
-          <div key={index} className={styles.card}>
-            <Link href={article.url} prefetch>
-                <img src={article.urlToImage} className={styles.image} />
-                <div className={styles.content}>
-                  <h3 className={styles.title}>{article.title}</h3>
-                  <p className={styles.description}>{article.description}</p>
-                </div>
-            </Link>
-          </div>
+      <div className={styles.articles}>
+        {articles.map((article) => (
+          <a
+            key={article.url}
+            href={article.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className={styles.article}>
+              <div className={styles.imageWrapper}>
+                <img
+                  src={article.urlToImage || "/no_image.png"}
+                  alt="thumbnail"
+                />
+              </div>
+              <div className={styles.contentWrapper}>
+                <p className={styles.title}>{article.title}</p>
+                <p className={styles.description}>{article.description}</p>
+                <p className={styles.source}>{article.source.name}</p>
+              </div>
+            </div>
+          </a>
         ))}
       </div>
     </div>
   );
-};
-
-export default News;
+}
